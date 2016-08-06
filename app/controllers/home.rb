387c3fb402cc8b5b45ca080baeb8e52be1c7b9d3
Elan Kvitko <elan@idknyc.com>
2016-08-06 11:47:24 -0400

    add json response to code path

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -7,4 +7,14 @@ get '/home' do
 end
 
 get '/code' do
+  code = ""
+
+  File.open( File.expand_path( __FILE__ ), "r" ) do | f |
+    f.each_line do | line |
+      code << line
+    end
+  end
+
+  content_type :json
+  { code: code }.to_json
 end
